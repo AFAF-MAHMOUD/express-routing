cc224a1d48fd1c60ae86127829d94d86ea9fe2fa
AFAF-MAHMOUD <afefmahmoud21031985@yahoo.ca>
2019-12-26 11:35:58 +0100

    message

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express")
 const app = express()
-const date = new Date();
-const hour= date.getHours();
-console.log(hour)
-if(hour>=8 && hour<=17){
-app.use(express.static(__dirname +"/html"))
-}else console.log("arja3 ghodwa")
 
-app.listen(3000, (err) => {
+
+app.use(Dates = (req, res, next) => {
+    const date = new Date();
+    const hour = date.getHours();
+    if (hour >= 12 && hour <= 17) {
+
+        app.use(express.static(__dirname + "/html"))
+    } else res.send ("arja3 ghodwa")
+    next()
+})
+
+
+app.listen(3001, (err) => {
     if (err) console.log("server not running");
     else console.log("server running on port 3000");
 })
