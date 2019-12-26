fb3bf0f0fb712b39b0532cda170a165bf38da94f
AFAF-MAHMOUD <afefmahmoud21031985@yahoo.ca>
2019-12-26 10:29:16 +0100

    limit hour of navigation

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
-
+const date = new Date();
+const hour= date.getHours();
+console.log(hour)
+if(hour>=8 && hour<=17){
 app.use(express.static(__dirname +"/html"))
-
+}else console.log("arja3 ghodwa")
 
 app.listen(3000, (err) => {
     if (err) console.log("server not running");
